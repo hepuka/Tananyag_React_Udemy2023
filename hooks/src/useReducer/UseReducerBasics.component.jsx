49642f099304299c259967b84fd87e5246542c01
Kun-Fagyal Zoltan <kunzoltan7707@gmail.com>
2023-03-19 18:34:15 +0100

    upd

--- hooks/src/useReducer/UseReducerBasics.component.jsx
+++ hooks/src/useReducer/UseReducerBasics.component.jsx
@@ -4,76 +4,81 @@ import React, { useReducer } from "react";
 // payload - sends some information to the reducer function
 
 const UseReducerBasics = () => {
+  //külön definiáljuk az initialstate-et és a reducer function-t a state-hez
   const initialState = {
     count: 0,
+    payload: 0,
+    mov: "",
   };
 
   const reducer = (state, action) => {
+    //az action-ben a dispatch-ra hivatkozunk
     switch (action.type) {
       case "SUBTRACT":
-        return { count: state.count - 1 };
+        return {
+          count: state.count - 1,
+          payload: action.payload,
+          mov: action.mov,
+        };
 
       case "ADD":
-        return { count: state.count + 1 };
+        return {
+          count: state.count + 1,
+          payload: action.payload,
+          mov: action.mov,
+        };
 
       case "RESET":
-        return { count: (state.count = 0) };
+        return {
+          count: (state.count = 0),
+          payload: action.payload,
+          mov: action.mov,
+        };
 
       default:
         return state;
 
       //throw new error() is lehet a default érték
     }
-
-    /*     //az action-ben a dispatch-ra hivatkozunkt
-    if (action.type === "SUBTRACT") {
-      //   console.log("Subtract action");
-      //   console.log(action.payload);
-
-      return {
-        count: state.count - 1,
-      };
-    }
-
-    if (action.type === "ADD") {
-      return {
-        count: state.count + 1,
-      };
-    }
-
-    if (action.type === "RESET") {
-      return {
-        count: (state.count = 0),
-      };
-    }
-
-    return state; */
   };
 
-  //const [count, setCount] = useState(0); nem kell ha van useReducer
+  //state az initialstate állapotát tárolja
+  // dispatch egy referencia arra, hogy milyen művelet hajtódjon végre amivel módosítjuk a state változót
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const handleSubtract = () => {
     //setCount(count - 1);
-    //const id = "12345";
+
+    const id = "11";
+    const movm = "subtract";
     dispatch({
       type: "SUBTRACT", //egy property-t meg kell adni
-      // payload: id, - opcionális
+      payload: id,
+      mov: movm,
+      /* - opcionális, ha innen az alfüggvényből pl. egy id-t is át akarunk adni a fő reducer függvénynek. Ott az action.payload-al tudunk erre hivatkozni*/
     });
   };
 
   const handleReset = () => {
     //setCount(0);
+    const id = "10";
+    const movm = "reset";
     dispatch({
       type: "RESET",
+      payload: id,
+      mov: movm,
     });
   };
 
   const handleAdd = () => {
     // setCount(count + 1);
+    const id = "12";
+    const movm = "adding";
     dispatch({
       type: "ADD",
+      payload: id,
+      mov: movm,
     });
   };
 
@@ -81,9 +86,11 @@ const UseReducerBasics = () => {
     <div>
       <section className="--flex-center --100vh --bg-primary">
         <div className="container --bg-light --p2 --m2 --center-all">
-          <h3>React Counter Page</h3>
+          <h3>React Counter with useReducer</h3>
 
-          <h1>{state.count}</h1>
+          <h1>Számláló: {state.count}</h1>
+          <h3>Művelet: {state.mov}</h3>
+          <h3>Művelet azonosító: {state.payload}</h3>
 
           <div className="buttons --flex-center">
             <button className="--btn --btn-danger" onClick={handleSubtract}>
